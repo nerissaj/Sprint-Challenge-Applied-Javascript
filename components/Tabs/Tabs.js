1727e5f55805318eeaddea5a2cad9172ad1f2157
Nerissa Taylor <taylornerissa@hotmail.com>
2019-06-22 17:18:23 -0400

    got everything to click

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -21,16 +21,16 @@ class TabLink {
 
     } else {
       // else if `all` is false, only select the cards with matching this.tabData values
-      this.cards = document.querySelector(`.card[data-tab="${this.tabData}"]`);
+      this.cards = document.querySelectorAll(`.card[data-tab="${this.tabData}"]`);
     }
 
     //console.log(this.cards);
     // Map over the newly converted NodeList we just created in our if statement above. Convert each this.cards element into a new instance of the TabCard class. Pass in a card object to the TabCard class. 
-    this.cards = Array.from(this.cards).map(card => TabCard + card);
+    this.cards = Array.from(this.cards).map(card => new TabCard(card));
     //console.log(this.cards);
     // Add a click event that invokes this.selectTab
     this.tabElement.addEventListener('click', () => 
-      this.selectTab());
+      {this.selectTab()});
 
     }
 
@@ -52,7 +52,7 @@ class TabLink {
     this.tabElement.classList.add('active-tab');
 
     // Notice we are looping through the this.cards array and invoking selectCard() from the TabCard class. Just un-comment the code and study what is happening here.
-    //this.cards.forEach(card => card.selectCard());
+    this.cards.forEach(card => card.selectCard());
 
   }
 }
